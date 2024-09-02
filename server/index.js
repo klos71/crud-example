00846b910d8935bc8cbf7a12d750edfa7e357737
Jonas Hellman <jonas.hellman@ericsson.com>
2024-09-02 13:38:08 +0200

    added simple crud

--- server/index.js
+++ server/index.js
@@ -2,30 +2,48 @@ const express = require("express");
 var cors = require("cors");
 
 const app = express();
-const port = 8080;
+const port = 8000;
 app.use(express.json());
 app.use(cors());
 
-app.get("/", (req, res) => {
-  res.json({ title: "This is some JSON data" });
-});
+const items = [];
 
 app.get("/", (req, res) => {
-  res.json([{ title: "This is some JSON data" }]);
+  res.json(items);
+});
+
+app.get("/:id", (req, res) => {
+  const item = items.find((i) => i.id === req.params.id);
+  if (item) {
+    res.json(item);
+  } else {
+    res.status(404).json({ message: "Not found" });
+  }
 });
 
 app.post("/", (req, res) => {
-  const { body } = req.body;
-  res.json({ title: "Hello from POST", body });
+  const { body } = req;
+  const len = items.push({ id: items.length, ...body });
+  res.json({ id: len - 1, ...body });
 });
 
-app.put("/", (req, res) => {
-  const { body } = req.body;
-  res.json({ title: "Hello from PUT", body });
+app.put("/:id", (req, res) => {
+  const { body } = req;
+  console.log(body, req.params.id);
+  const index = items.findIndex((i) => i.id == req.params.id);
+
+  if (index >= 0) {
+    items[index].value = body.value;
+    res.json(items[index]);
+  } else {
+    res.status(404).json({ message: "Not found" });
+  }
 });
 
 app.delete("/:id", (req, res) => {
-  res.json({ title: "Delete", id });
+  const index = items.find((i) => i.id === req.params.id);
+  items.splice(index, 1);
+  res.json(items);
 });
 
 app.listen(port, () => {
